37eabc74da6750a7beea38a725176c95be53839b
LeppoXena <francesco.lepore@xenagames.com>
2023-02-08 16:36:38 +0100

    Second (milestone 3)

--- src/main/java/db/nation/Main.java
+++ src/main/java/db/nation/Main.java
@@ -14,29 +14,31 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-//import java.util.Scanner;
+import java.util.Scanner;
 
 	public class Main {
 		public static void main(String[] args) throws SQLException{
 
-		
 			 String url = "jdbc:mysql://localhost:3306/db-nations";
 			 String user = "root";
 			 String password = "root";
-//			 Scanner s=new Scanner(System.in);
-			  
-			 
-			
+			 String input = "";
+			 Scanner s=new Scanner(System.in);			 
+			 System.out.println("Inserisci il nome del paese o parte di esso");
+			 input = (s.nextLine());
 			  try (Connection con = DriverManager.getConnection(url, user, password)){
 				  
 				  String sql="select c.name, c.country_id , r.name , c2.name  from countries c \r\n"
 				  		+ "join regions r on c.region_id = r.region_id \r\n"
 				  		+ "join continents c2 on c2.continent_id = r.continent_id \r\n"
+				  		+ "where c.name like ?"
 				  		+ "order by c.name asc";
 				 
 				  try(PreparedStatement ps=con.prepareStatement(sql)) {
 					  
-					  try(ResultSet rs =ps.executeQuery()) {
+					  ps.setString(1, "%"+input+"%");
+					  
+					  try(ResultSet rs = ps.executeQuery()) {
 						  
 						  System.out.println("");
 						  while (rs.next()) { //se c'è qualcosa da leggere
@@ -54,8 +56,7 @@ import java.sql.SQLException;
 			     ex.printStackTrace();
 			  }
 
-			
-		
+			s.close();		
 		}
 
 }
